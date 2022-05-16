3ee1c3f2698f5088fdc977d6ce66a94ffa6f7e0f
Naga <105265405+9600166807@users.noreply.github.com>
2022-05-16 12:27:52 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -3,5 +3,6 @@ public class HelloGuys {
   public static void main(String{} args) {
     System.out.println("Hey Naga");
     System.out.println("Hey Karthik");
+    System.out.println("Hey Nagarthik");
   }
 }
